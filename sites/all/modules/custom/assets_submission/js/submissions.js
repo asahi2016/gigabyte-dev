59c5c48b8064a77abf11f64c45c9cde6c04b32cc
krishna <krishna@asahimails.com>
2016-03-31 16:13:34 +0530

    GIG-112 Create a new block for render submission content type form.

--- sites/all/modules/custom/assets_submission/js/submissions.js
+++ sites/all/modules/custom/assets_submission/js/submissions.js
@@ -1,15 +1,13 @@
-/**
- * Created by asahi-qa2 on 31/3/16.
- */
 (function ($) {
     $(document).ready(function($) {
-            var new_submission = $('div#new-submission');
-            $('div#new-submission').remove();
-            $('form#submission-node-form').prepend(new_submission);
-            $('form#submission-node-form').hide();
 
-            $(document).on('click','div#new-submission', function () {
-                $('form#submission-node-form').toggle();
-            });
+        var new_submission = $('div.new-submission-form');
+        $('form#submission-node-form').parent('.content').prepend(new_submission);
+        //$('div.new-submission-form').remove();
+        $('form#submission-node-form').hide();
+
+        $(document).on('click','input#new-submission', function () {
+            $('form#submission-node-form').toggle();
+        });
     });
 })(jQuery);
