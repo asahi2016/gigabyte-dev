8c8211a5394b2843e321f6547faffb47fdce437f
arun733 <arung.asahi@gmail.com>
2016-04-05 15:08:00 +0530

    GIG-132 Reset button initail implementation

---
+++ sites/all/modules/custom/project_support_request/js/project_request.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $(document).ready(function($){
+        alert('yuo');
+        $('#edit-reset').click(function(){
+            alert('hi');
+            $('#webform-client-form-76').reset();
+        });
+
+
+
+        });
+
+});
