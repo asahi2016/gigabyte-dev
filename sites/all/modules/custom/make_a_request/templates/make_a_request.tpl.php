58a31a2b9b62908319ab927e3fe25f91ac89ff0b
arun733 <arung.asahi@gmail.com>
2016-04-12 17:42:18 +0530

    GIG-133 Export Excel issue fixed

--- sites/all/modules/custom/make_a_request/templates/make_a_request.tpl.php
+++ sites/all/modules/custom/make_a_request/templates/make_a_request.tpl.php
@@ -4,6 +4,7 @@
 global $base_url;
 
 drupal_add_js($base_url.'/sites/all/modules/custom/make_a_request/js/jquery-1.12.0.min.js');
+drupal_add_js($base_url.'/sites/all/modules/custom/make_a_request/js/jquery.table2excel.js');
 drupal_add_js($base_url.'/sites/all/modules/custom/make_a_request/js/dataTables.bootstrap.min.js');
 drupal_add_js($base_url.'/sites/all/modules/custom/make_a_request/js/jquery.dataTables.min.js');
 drupal_add_js($base_url.'/sites/all/modules/custom/make_a_request/js/dataTables.buttons.min.js');
@@ -17,19 +18,21 @@ drupal_add_css($base_url.'/sites/all/modules/custom/make_a_request/css/jquery.da
 drupal_add_css($base_url.'/sites/all/modules/custom/make_a_request/css/buttons.dataTables.min.css');
 
 ?>
-<div>
+<button id="export-excel">Export</button>
+<div class="request-data">
     <table id="pagination" class="table table-striped table-bordered" cellspacing="0" width="100%">
         <thead>
-        <th>Date</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Contact Email</th>
-        <th>Company Name</th>
-        <th>Request</th>
+            <tr>
+                <th>Date</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Contact Email</th>
+                <th>Company Name</th>
+                <th>Request</th>
+            </tr>
         </thead>
         <tbody>
         <?php foreach($value as $customer_request){?>
-
             <tr>
                 <td><?php echo $customer_request->date;?></td>
                 <td><?php echo $customer_request->first_name;?></td>
@@ -41,21 +44,25 @@ drupal_add_css($base_url.'/sites/all/modules/custom/make_a_request/css/buttons.d
         <?php } ?>
         </tbody>
     </table>
+    <?php print $exportTable; ?>
 </div>
 <script type='text/javascript'>
 
     $(document).ready(function() {
+
+        $('#export-excel').click(function(){
+            $("#pagination-none").table2excel({
+                exclude: ".noExl",
+                name: "Worksheet Name",
+                filename: "Make a Request" //do not include extension
+            });
+        });
+
         $('#pagination').DataTable( {
             jQueryUI: true,
-            order:[[0,'desc']],
-            dom: 'Bfrtip',
-            buttons: [
-                { extend: 'excel', text: 'Export Request Review to excel' },
-                { extend: 'csv', text: 'Export Request Review to csv' },
-                { extend: 'excelFlash', text: 'Export Request to excel' },
-                { extend: 'csvFlash', text: 'Export Request  to csv' }
-            ]
-        } );
-    } );
+            order:[[0,'desc']]
+        });
+
+    });
 
 </script>
