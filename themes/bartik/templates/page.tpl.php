1f44da98839313b912e317e198877beff8cab4b9
Avinash <avinash@asahitechnologies.com>
2016-04-20 17:35:25 +0530

    GIG-152 Assets upload form implemented with dpi for each upload

--- themes/bartik/templates/page.tpl.php
+++ themes/bartik/templates/page.tpl.php
@@ -1,5 +1,7 @@
 <script type="text/javascript">
     jQuery(document).ready(function($) {
+
+
         $('#scrol-top').click(function() {
            $("html,body").animate({scrollTop: 0}, 1000, 'swing');
         });
@@ -172,9 +174,45 @@ if(!user_is_logged_in()){
     }
 }
 
-drupal_add_js("jQuery(document).ready(function(){country_id = " . (!empty($_SESSION['user_country_id']) ? $_SESSION['user_country_id'] : 1) . "; country_content = '".(!empty($node_content)?$node_content:'')."';country = location.search.split('country=')[1]; if((country == 'ca' || country_id == 2) && country_content.length>0){jQuery('#page-canada-content').show();jQuery('#page-us-content').hide();jQuery('#country-menu li:first-child').removeClass('active');jQuery('#country-menu li:last-child').addClass('active');}else if(country_id == 1){jQuery('#page-canada-content').hide();jQuery('#page-us-content').show();if(country == 'us'){jQuery('#country-menu li:first-child').addClass('active');jQuery('#country-menu li:last-child').removeClass('active');}else if(country == 'ca'){jQuery('#country-menu li:first-child').removeClass('active');jQuery('#country-menu li:last-child').addClass('active');}else{jQuery('#country-menu li:first-child').addClass('active');jQuery('#country-menu li:last-child').removeClass('active');}}else{jQuery('#page-canada-content').hide();jQuery('#page-us-content').show();if(country == 'us' || country_id == 1){jQuery('#country-menu li:first-child').addClass('active');jQuery('#country-menu li:last-child').removeClass('active');}else if(country == 'ca' || country_id == 2){jQuery('#country-menu li:first-child').removeClass('active');jQuery('#country-menu li:last-child').addClass('active');}else{jQuery('#country-menu li:first-child').addClass('active');jQuery('#country-menu li:last-child').removeClass('active');}}})", array('type' => 'inline','scope' => 'footer'));
-
 ?>
+<script type="text/javascript">
+jQuery(document) . ready(function () {
+        country_id = <?php echo (!empty($_SESSION['user_country_id']) ? $_SESSION['user_country_id'] : 1) ?>;
+        country_content = "<?php echo (!empty($node_content)?$node_content:'') ?>";
+        country = location.search.split('country=')[1];
+        if ((country == 'ca' || country_id == 2) && country_content . length > 0) {
+            jQuery('#page-canada-content') . show();
+            jQuery('#page-us-content') . hide();
+            jQuery('#country-menu li:first-child') . removeClass('active');
+            jQuery('#country-menu li:last-child') . addClass('active');
+        } else if (country_id == 1) {
+            jQuery('#page-canada-content') . hide();
+            jQuery('#page-us-content') . show();
+            if (country == 'us') {
+                jQuery('#country-menu li:first-child') . addClass('active');
+                jQuery('#country-menu li:last-child') . removeClass('active');
+            } else if (country == 'ca') {
+                jQuery('#country-menu li:first-child') . removeClass('active');
+                jQuery('#country-menu li:last-child') . addClass('active');
+            } else {
+                jQuery('#country-menu li:first-child') . addClass('active');
+                jQuery('#country-menu li:last-child') . removeClass('active');
+            }
+        } else {
+            jQuery('#page-canada-content') . hide();
+            jQuery('#page-us-content') . show();
+            if (country == 'us' || country_id == 1) {
+                jQuery('#country-menu li:first-child') . addClass('active');
+                jQuery('#country-menu li:last-child') . removeClass('active');
+            } else if (country == 'ca' || country_id == 2) {
+                jQuery('#country-menu li:first-child') . removeClass('active');
+                jQuery('#country-menu li:last-child') . addClass('active');
+            } else {
+                jQuery('#country-menu li:first-child') . addClass('active');
+                jQuery('#country-menu li:last-child') . removeClass('active');
+            }
+        }})
+</script>
 <div id="page-wrapper"><div id="page">
 
   <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>">
