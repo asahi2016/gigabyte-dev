60a139003660572e523e9a8a1995b66fc6e1cd28
arun733 <arung.asahi@gmail.com>
2016-04-21 17:58:18 +0530

    GIG - 203 How to sell upload form created.
    And sereis based on platform has been implemented.

--- sites/all/modules/custom/how_to_sell/js/how_to_sell.js
+++ sites/all/modules/custom/how_to_sell/js/how_to_sell.js
@@ -1,6 +1,28 @@
 (function ($) {
     $(document).ready(function($) {
 
+        var current_val = $('#edit-field-platform-und').val();
+        if (current_val == 32) {
+            $("#edit-field-series-und").html("<option value='34'>100 Series</option>" +
+            "<option value='35'>X99 Series</option>" +
+            "<option value='36'>9 Series</option>" +
+            "<option value='37'>8 Series</option>");
+        }
+
+        $('#edit-field-platform-und').change(function(){
+            var current_val = $(this).val();
+            if (current_val == 32) {
+                $("#edit-field-series-und").html("<option value='34'>100 Series</option>" +
+                "<option value='35'>X99 Series</option>" +
+                "<option value='36'>9 Series</option>" +
+                "<option value='37'>8 Series</option>");
+        }
+            else if (current_val == 33) {
+                $("#edit-field-series-und").html("<option value='39'>900 Series</option>");
+            }
+
+        });
+
 
     });
 })(jQuery);
