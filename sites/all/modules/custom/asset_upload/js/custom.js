e08f681e0e447baea4f89d4a664ec14ccedb48ea
Avinash <avinash@asahitechnologies.com>
2016-04-26 18:33:24 +0530

    GIG-159 Download history page implemented

--- sites/all/modules/custom/asset_upload/js/custom.js
+++ sites/all/modules/custom/asset_upload/js/custom.js
@@ -7,7 +7,53 @@ jQuery(document).ready(function($){
             'paging':true
         });
     });*/
-    $('.assets-preview-image').colorbox();
-    $('.field-name-field-upload-jpg .field-label').html('Download:');
+    $('.assets-preview-image').each(function(){
+        imghref = $(this).find('img').attr('src');
+        $(this).find('img').attr('href',imghref);
+    });
+    $(document).on('load','body',function(){
+        $('.assets-preview-image').each(function(){
+            imghref = $(this).find('img').attr('src');
+            $(this).find('img').attr('href',imghref);
+        });
+    });
+
+    $('.assets-preview-image img').colorbox();
     $('.assets-upload-display img.file-icon').hide();
+
+
+    $("ul.assets-list-partner li.assets-row-list-partner").each(function(){
+        $(this).find('div.views-field-field-upload-file').find('table.download-file-type-info-table').find('td:first-child').click(function(){
+            var imgpath = $(this).find('a').attr('href');
+            $.ajax({
+                url:Drupal.settings.gigabyte.baseUrl+'/partner/asset/store/history',
+                type:'post',
+                data:{image:imgpath},
+                success:function(data){
+                    alert(data);
+                    if(data > 0){
+                        console.log('Download Saved');
+                    }
+                }
+            });
+        });
+    });
+
+
+    function file_ajax_save(imgpath){
+        $.ajax({
+            url:Drupal.settings.gigabyte.baseUrl+'/partner/asset/store/history',
+            type:'post',
+            data:{image:imgpath},
+            success:function(data){
+                alert(data);
+                if(data > 0){
+                    console.log('Download Saved');
+                }
+            }
+        });
+    }
+
+
 });
+
