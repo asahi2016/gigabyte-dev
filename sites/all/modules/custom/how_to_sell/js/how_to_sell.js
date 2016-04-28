8a5c34722f6505c826abf696178562967d24d4f5
arun733 <arung.asahi@gmail.com>
2016-04-28 09:20:55 +0530

    GIG - 209- Implement the color box to view pdf file

--- sites/all/modules/custom/how_to_sell/js/how_to_sell.js
+++ sites/all/modules/custom/how_to_sell/js/how_to_sell.js
@@ -24,60 +24,11 @@
 
         });
 
-        $('.filter-tab a').on('click', function(e) {
-            e.preventDefault();
+        setInterval(function(){
+            $(".views-field-field-file-image .field-content a").colorbox({ iframe: true, width: "90%", height: "95%" });
+        },600);
 
-            // Get ID of clicked item
-
-            var id = $(e.target).attr('id');
-
-
-            // Set the new value in the SELECT element
-          var filter = $('#views-exposed-form-how-to-sell-view-page select[name="term_node_tid_depth"]');
-            filter.val(id);
-
-            // Unset and then set the active class
-            $('.filter-tab a').removeClass('active');
-            $(e.target).addClass('active');
-            // Do it! Trigger the select box
-            //filter.trigger('change');
-            $('#views-exposed-form-how-to-sell-view-page select[name="term_node_tid_depth"]').trigger('change');
-            $('#views-exposed-form-how-to-sell-view-page input.form-submit').trigger('click');
-
-
-
-
-        });
-        jQuery(document).ajaxComplete(function(event, xhr, settings) {
-
-            switch(settings.extraData.view_name){
-
-                case "how_to_sell_view":
-                    var filter_id = $('#views-exposed-form-how-to-sell-view-page select[name="term_node_tid_depth"]').find(":selected").val();
-
-                    $('.filter-tab a').removeClass('active');
-                    $('.filter-tab').find('#' + filter_id).addClass('active');
-
-                    break;
-
-            };
-        });
-
-        $('.view-how-to-sell-view .field-content a .cke_colorbox').colorbox({ iframe: true, width: "90%", height: "95%" });
-
-
-        /*var pdf = $('.views-field-field-file-pdf .content').wrap('<a href=""');
-        //alert(pdf);
-        $('.views-field-field-file-image .field-content a')
-*/
-
-
-
-
-
-
-
-
+        $(".views-field-field-file-image .field-content a").colorbox({ iframe: true, width: "90%", height: "95%" });
 
     });
 })(jQuery);
