989b2d7aa1f40f3af088dceafd7351b89ffda7a0
krishna <krishna@asahimails.com>
2016-04-29 10:46:39 +0530

    GIG-218 Awards and Reviews Banner image section implement

--- sites/all/modules/custom/awards_reviews/js/awards_reviews.js
+++ sites/all/modules/custom/awards_reviews/js/awards_reviews.js
@@ -1,7 +1,28 @@
 (function ($) {
     $(document).ready(function($) {
 
+         $('#views-exposed-form-awards-reviews-page a').click(function () {
+             $.post(
+                 Drupal.settings.gigabyte.baseUrl + '/get/awards_and_reviews/banner',
+                 {
+                     term_id : getParameterByName('term_node_tid_depth', $(this).attr('href')),
+                     ajax: true
+                 },
+                 function (response) {
 
+                 }
+             );
+         });
+
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
 
     });
 })(jQuery);
