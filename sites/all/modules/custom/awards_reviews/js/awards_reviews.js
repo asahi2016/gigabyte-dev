b32e1ecec06c6f3296890108e4669615150a220a
krishna <krishna@asahimails.com>
2016-04-29 20:24:30 +0530

    GIG-219 Display Awards and Reviews page banner based on Filters series.

--- sites/all/modules/custom/awards_reviews/js/awards_reviews.js
+++ sites/all/modules/custom/awards_reviews/js/awards_reviews.js
@@ -2,11 +2,13 @@
     $(document).ready(function($) {
          $('#edit-term-node-tid-depth-all').remove();
 
-         var uri = $('.content .node-page').attr('about');
-         var class_node = $('.content .node-page').attr('id');
+         if($('.content .node-page').attr('about').length > 0) {
+             var uri = $('.content .node-page').attr('about');
+             var class_node = $('.content .node-page').attr('id');
 
-         if(uri.indexOf('admin/awards-reviews') > -1){
-            $('#'+class_node+' .group-page-us-content .field-label-hidden p').hide();
+             if (uri.indexOf('admin/awards-reviews') > -1) {
+                 $('#' + class_node + ' .group-page-us-content .field-label-hidden p').hide();
+             }
          }
 
          $('#views-exposed-form-awards-reviews-page .description').remove();
