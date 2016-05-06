e2df86b6823ef8bafca909a23a03cfe19ff1e661
arun733 <arung.asahi@gmail.com>
2016-05-06 09:26:38 +0530

    GIG- 18 - Implement each search result count.

--- sites/all/modules/custom_search/theme/custom_search-results.tpl.php
+++ sites/all/modules/custom_search/theme/custom_search-results.tpl.php
@@ -21,6 +21,7 @@
  */
 ?>
 <h2><?php print t('Search results');?></h2>
+<h2><?php print t('Results '.$all_results.' Items')?></h2>
 <?php if (isset($filter) && $filter != '' && $filter_position == 'above') : ?>
     <div class="custom-search-filter">
         <?php print $filter; ?>
