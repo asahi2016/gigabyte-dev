4dbc0ba2b9c5e3d75e2429f4722e8ec36741efec
Avinash <avinash@asahitechnologies.com>
2016-05-10 17:52:00 +0530

    GIG-22 recommit after resolving conflict and merging changes

--- sites/all/modules/custom/e_catalogs/js/custom.js
+++ sites/all/modules/custom/e_catalogs/js/custom.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function($){
+    alert('here');
+    //Colorbox Popup Load for E-Catalogs
+    //$('a.catalog-popup-link img').attr('href');
 
+    $('div.view-e-catalogs-view ul li.views-row').each(function(){
+        $(this).find('a.catalog-popup-link').colorbox({ href:$(this).find('a.catalog-popup-link').attr('href'),iframe: true, width: "90%", height: "95%" });
+    });
 });
 
