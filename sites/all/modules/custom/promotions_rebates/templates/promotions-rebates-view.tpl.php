8bbeda88b006e23dc6ba268dad47ce2d3ba6f79f
krishna <krishna@asahimails.com>
2016-05-25 17:39:19 +0530

    GIG-291 Display individual promotions based on distributors

--- sites/all/modules/custom/promotions_rebates/templates/promotions-rebates-view.tpl.php
+++ sites/all/modules/custom/promotions_rebates/templates/promotions-rebates-view.tpl.php
@@ -1,20 +1,12 @@
 <script>
     jQuery(document).ready(function($) {
-
-        $(".promo").hide();
-
-        $('.distributor').click(function () {
-            $(".promo").hide();
-            var term = $(this).attr('term');
-            $(term).show();
-
-        });
+       
     });
 </script>
-<div class="txt_left"><a href="http://www.gigabyte.us/" target="_blank"><img src="../GIGABYTE_main-page_01.jpg" /></a></div>
-<?php $uri = isset($node->field_promotion_banner['und'][0]['uri'])? file_create_url($node->field_promotion_banner['und'][0]['uri']):''; ?>
-<div><img src="<?php print $uri; ?>" /></div>
-
+<div class="txt_center"><a href="http://www.gigabyte.us/" target="_blank"><img src="../GIGABYTE_main-page_01.jpg" /></a></div>
+<?php $uri = isset($node->field_promotion_banner['und'][0]['uri'])? file_create_url($node->field_promotion_banner['und'][0]['uri']):'';?>
+<div class="txt_center"><img src="<?php print $uri;?>" style="width: 1000px;height:400px;margin: 20px 0px;"></div>
+<?php global $base_url; ?>
 <?php if(isset($variables['terms'])) { ?>
     <table class="stores">
     <tbody>
@@ -22,7 +14,7 @@
     <?php foreach ($terms as $id => $term) {
         $uri = isset($term->term->field_distributor_image['und'][0]['uri'])? file_create_url($term->term->field_distributor_image['und'][0]['uri']):'';
         ?>
-        <td><img src="<?php print $uri;?>" term='#term<?php print $term->tid;?>' class="distributor"/></td>
+        <td><a href="<?php print $base_url .'/' .drupal_get_path_alias() .'/'. $term->name; ?>" target="_blank"><img src="<?php print $uri;?>" term='#term<?php print $term->tid;?>' class="distributor"/></a></td>
         <?php
     } ?>
         </tr>
@@ -61,7 +53,7 @@
             <!--<div style="text-align:right; font-size:10px; width:800px;">*Instant Rebate Valid from January 18-22.
                 Promotion cannot be combined with any other offer.
             </div>-->
-            <table style="width:900px; margin-top:20px;">
+            <table class="product_notes">
                 <tbody>
             <?php
             $img_url = isset($term['term']->field_distributor_image['und'][0]['uri'])? file_create_url($term['term']->field_distributor_image['und'][0]['uri']):'';
@@ -70,7 +62,7 @@
                 ?>
                 <tr>
                     <td><a href="<?php print $term_url;?>" target="_blank"><img src="<?php print $img_url; ?>"/></a></td>
-                    <td style="font-size:16px;"><?php print $note; ?></td>
+                    <td><?php print $note; ?></td>
                 </tr>
             <?php } ?>
                 </tbody>
