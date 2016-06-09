8a90439cb8abea0a8053059a6acebe7a44167b62
krishna <krishna@asahimails.com>
2016-06-09 12:35:28 +0530

    GIG-341 Import Excel issue fixed

--- sites/all/modules/custom/promotions_rebates/js/promotions_rebates.js
+++ sites/all/modules/custom/promotions_rebates/js/promotions_rebates.js
@@ -1,81 +1,23 @@
 (function ($) {
     $(document).ready(function($) {
 
-        var access = true;
-        $(window).on('load','form.node-form node-promotion_rebates-form',function(){
-            alert('2');
-            alert($('select[id*="edit-field-distributor-promotion-deta"]').attr('selected').val());
-        });
-            /*img_url = '';
-            $('select[id*="edit-field-distributor-promotion-deta"]').parent('div').removeClass('active-selected');
-            $(this).parent('div').addClass('active-selected');
-            $.ajax({
-                url:Drupal.settings.gigabyte.baseUrl+'/promotions/get/termdata',
-                type:'post',
-                data:{termid:$(this).val()},
-                success:function(data){
-                    img_url = data;
-                    var html_content = '<span class="distributor-image" ><img src="'+img_url+'" /> </span>';
-                    $('div.field-name-field-prmotion-distributors div.active-selected').find('span').remove();
-                    $('div.field-name-field-prmotion-distributors div.active-selected').append(html_content);
-                }
-            });*/
-
-        /*setInterval(function () {
+        setInterval(function () {
 
             if($('#promotion-rebates-node-form').find('#edit-field-promotion-excel').find('span.file').length > 0){
-
-                if(access){
-
-                    access = false;
-
-                    $.post(
-                        Drupal.settings.gigabyte.baseUrl + '/promotions/ajax',
-                        {
-                            ajax: true
-                        },
-                        function (response) {
-                            alert(response);
-                        }
-                    );
-                }
+                $('table[id*="field-distributor-promotion-deta-values"]').hide();
+                $('#edit-field-distributor-promotion-deta-und-add-more').hide();
+            }else{
+                $('table[id*="field-distributor-promotion-deta-values"]').show();
+                $('#edit-field-distributor-promotion-deta-und-add-more').show();
             }
-        },100);*/
-       // $("#edit-field-distributor-promotion-deta-und-add-more").trigger("click");
+        },100);
 
-
-       /* if($('span.file').length > 0) {
-
-        }else{
-           /*  $.post(
-                Drupal.settings.gigabyte.baseUrl + '/promotions/ajax',
-                {
-                    ajax: true
-
-                },
-                function (response) {
-                    $('#edit-field-distributor-promotion-deta').html('');
-                    $('#edit-field-distributor-promotion-deta').html(response);
-                }
-
-            );
-        }
-
-            );} */
-        
-
-
-
-        /*$('#promotion-rebates-node-form').on('submit', function(e) {
-            //prevent the default submithandling
-            e.preventDefault();
-            //send the data of 'this' (the matched form) to yourURL
-         $.post('url' , array('formval' => $(this).serialize()));
-        });*/
-        setInterval(function(){
+        /*setInterval(function(){
             console.log($('select[id*="edit-field-distributor-promotion-deta]').attr('selected','selected').val());
             //gettermimage($('select[id*="edit-field-distributor-promotion-deta]').attr('selected','selected').val());
-        },2000);
+        },2000);*/
+
+        //Display distributor image on selection from dropdown
         $(document).on('change','select[id*="edit-field-distributor-promotion-deta"]',function(){
             img_url = '';
             $('select[id*="edit-field-distributor-promotion-deta"]').parent('div').removeClass('active-selected');
@@ -103,7 +45,6 @@
                 data:{termid:termid},
                 success:function(data){
                     img_url = data;
-                    alert(data);
                     var html_content = '<span class="distributor-image" ><img src="'+img_url+'" /> </span>';
                     $('div.field-name-field-prmotion-distributors div.active-selected').find('span').remove();
                     $('div.field-name-field-prmotion-distributors div.active-selected').append(html_content);
