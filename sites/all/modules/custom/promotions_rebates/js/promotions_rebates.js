89cbf2271a120ad271f073dc4623b21f3ff2bbd1
AsahiTechnologies <badgeos@asahitechnologies.com>
2016-06-09 11:59:28 +0530

    Promotion rebate js commit

--- sites/all/modules/custom/promotions_rebates/js/promotions_rebates.js
+++ sites/all/modules/custom/promotions_rebates/js/promotions_rebates.js
@@ -2,6 +2,24 @@
     $(document).ready(function($) {
 
         var access = true;
+        $(window).on('load','form.node-form node-promotion_rebates-form',function(){
+            alert('2');
+            alert($('select[id*="edit-field-distributor-promotion-deta"]').attr('selected').val());
+        });
+            /*img_url = '';
+            $('select[id*="edit-field-distributor-promotion-deta"]').parent('div').removeClass('active-selected');
+            $(this).parent('div').addClass('active-selected');
+            $.ajax({
+                url:Drupal.settings.gigabyte.baseUrl+'/promotions/get/termdata',
+                type:'post',
+                data:{termid:$(this).val()},
+                success:function(data){
+                    img_url = data;
+                    var html_content = '<span class="distributor-image" ><img src="'+img_url+'" /> </span>';
+                    $('div.field-name-field-prmotion-distributors div.active-selected').find('span').remove();
+                    $('div.field-name-field-prmotion-distributors div.active-selected').append(html_content);
+                }
+            });*/
 
         /*setInterval(function () {
 
@@ -54,9 +72,14 @@
             //send the data of 'this' (the matched form) to yourURL
          $.post('url' , array('formval' => $(this).serialize()));
         });*/
-
-        /*$(document).on('change','select[id*="edit-field-distributor-promotion-deta-und-0-field-prmotion-distributors-und"]',function(){
+        setInterval(function(){
+            console.log($('select[id*="edit-field-distributor-promotion-deta]').attr('selected','selected').val());
+            //gettermimage($('select[id*="edit-field-distributor-promotion-deta]').attr('selected','selected').val());
+        },2000);
+        $(document).on('change','select[id*="edit-field-distributor-promotion-deta"]',function(){
             img_url = '';
+            $('select[id*="edit-field-distributor-promotion-deta"]').parent('div').removeClass('active-selected');
+            $(this).parent('div').addClass('active-selected');
             $.ajax({
                 url:Drupal.settings.gigabyte.baseUrl+'/promotions/get/termdata',
                 type:'post',
@@ -64,11 +87,29 @@
                 success:function(data){
                     img_url = data;
                     var html_content = '<span class="distributor-image" ><img src="'+img_url+'" /> </span>';
-                    $('select[id*="edit-field-distributor-promotion-deta-und-0-field-prmotion-distributors-und"]').parent('div').find('span').remove();
-                    $('select[id*="edit-field-distributor-promotion-deta-und-0-field-prmotion-distributors-und"]').parent('div').append(html_content);
+                    $('div.field-name-field-prmotion-distributors div.active-selected').find('span').remove();
+                    $('div.field-name-field-prmotion-distributors div.active-selected').append(html_content);
+                }
+            });
+        });
+
+        function gettermimage(termid){
+            img_url = '';
+            $('select[id*="edit-field-distributor-promotion-deta"]').parent('div').removeClass('active-selected');
+            $(this).parent('div').addClass('active-selected');
+            $.ajax({
+                url:Drupal.settings.gigabyte.baseUrl+'/promotions/get/termdata',
+                type:'post',
+                data:{termid:termid},
+                success:function(data){
+                    img_url = data;
+                    alert(data);
+                    var html_content = '<span class="distributor-image" ><img src="'+img_url+'" /> </span>';
+                    $('div.field-name-field-prmotion-distributors div.active-selected').find('span').remove();
+                    $('div.field-name-field-prmotion-distributors div.active-selected').append(html_content);
                 }
             });
 
-        });*/
+        }
     });
 })(jQuery);
